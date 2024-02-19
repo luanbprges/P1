6a8fe7f673d72f50deabc13f58ed11e9db8342b3
luanbprges <116804556+luanbprges@users.noreply.github.com>
2024-02-19 18:29:50 -0300

    Add files via upload

---
+++ Q2/P1Q2/P1Q2/NumComplexo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace P1Q2
+{
+    internal class NumComplexo
+    {
+        //Atributos
+
+        double re;
+        double im;
+
+        public double Re { get => re; set => re = value; }
+        public double Im { get => im; set => im = value; }
+
+
+        //Metodos
+
+        public NumComplexo()
+        {
+            Re = 0;
+            Im = 0;
+        }
+
+        public NumComplexo(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        //letra a)
+
+        public NumComplexo soma(NumComplexo complexo)
+        {
+            NumComplexo somaComplexa = new NumComplexo();
+
+            somaComplexa.Re = Re + complexo.Re;
+            somaComplexa.Im = Im + complexo.Im;
+
+            return somaComplexa;
+        }
+
+        //letra b)
+
+        public NumComplexo vezes(NumComplexo complexo)
+        {
+            NumComplexo multComplexa = new NumComplexo();
+
+            multComplexa.Re = (Re * complexo.Re) - (Im * complexo.Im);
+            multComplexa.Im = (Re * complexo.Im) + (complexo.Re * Im);
+
+            return multComplexa;
+        }
+
+        //letra c)
+
+        public double modulo(NumComplexo complexo)
+        {
+            double modComplex = 0;
+
+            modComplex = Math.Sqrt((Im * Im) + (Re * Re));
+
+            return modComplex;
+        }
+
+        //letra d)
+
+        public double Argumento()
+        {
+            double angulo = Math.Acos(Re / Math.Sqrt((Im * Im) + (Re * Re)));
+
+            return angulo;
+        }
+
+        //letra e)
+
+        public void ImprimeFormaPolar()
+        {
+            double p = Math.Sqrt((Im * Im) + (Re * Re));
+            double angulo = Math.Acos(Re / Math.Sqrt((Im * Im) + (Re * Re)));
+
+            Console.WriteLine(p + "(cos(" + angulo + ") + isen(" + angulo + ")) \n");
+        }
+    }
+}
---
+++ Q2/P1Q2/P1Q2/Program.cs
@@ -0,0 +1,23 @@
+﻿using P1Q2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //letra f)
+
+        NumComplexo z1 = new NumComplexo(1, 1);
+        NumComplexo z2 = new NumComplexo(3, (-1));
+        NumComplexo somaComplex = new NumComplexo();
+        NumComplexo multComplex = new NumComplexo();
+
+        somaComplex = z1.soma(z2);
+        multComplex = z1.vezes(z2);
+
+        Console.WriteLine("Soma na forma polar: ");
+        somaComplex.ImprimeFormaPolar();
+
+        Console.WriteLine("Multiplicação na forma polar: ");
+        multComplex.ImprimeFormaPolar();
+    }
+}
